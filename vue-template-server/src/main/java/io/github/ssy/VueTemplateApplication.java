136460e8fe5d8dd7746fe864fecde36c2d547c5c
StriveStruggleYou <StriveStruggleYou@163.com>
2019-11-04 15:22:45 +0800

    提交基础逻辑信息

---
+++ vue-template-server/src/main/java/io/github/ssy/VueTemplateApplication.java
@@ -0,0 +1,13 @@
+package io.github.ssy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueTemplateApplication {
+
+  public static void main(String arg[]) {
+    SpringApplication.run(VueTemplateApplication.class, arg);
+  }
+
+}
